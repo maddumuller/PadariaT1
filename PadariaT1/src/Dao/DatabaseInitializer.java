1e19614f7cd115c205ad0bb72f44b3d92ae4d051
duda <mullerermesm@gmail.com>
2025-05-27 11:09:07 -0300

    tabelas de inicializaçao

--- PadariaT1/src/Dao/DatabaseInitializer.java
+++ PadariaT1/src/Dao/DatabaseInitializer.java
@@ -35,6 +35,43 @@ public class DatabaseInitializer {
                 stmt.executeUpdate(tabelaCliente);
                 System.out.println("Tabela cliente criado com sucesso!");
 
+                String tabelaVenda = "CREATE TABLE IF NOT EXISTS venda("+
+                        "id SERIAL PRIMARY KEY,"+
+                        "cliente_id INT,"+
+                        "data_venda TIMESTAMP DEFAULT CURRENT,"+
+                        "total_valor NUMERIC(10,2) NOT NULL,"+
+                        "pontos_gerados INT NOT NULL,"+
+                        "FOREIGN KEY (cliente_id) REFERENCES Cliente(id)"+");";
+
+                stmt.executeUpdate(tabelaVenda);
+                System.out.println("Tabela venda criado com sucesso!");
+
+                String tabelaVendaProduto = "CREATE TABLE IF NOT EXISTS Venda_Produto ("+
+                        "venda_id INT NOT NULL,"+
+                        "produto_id INT NOT NULL,"+
+                        "quantidade INT NOT NULL,"+
+                        "preco_unitario NUMERIC(10,2) NOT NULL,"+
+                        "PRIMARY KEY (venda_id, produto_id),"+
+                        "FOREIGN KEY (venda_id) REFERENCES Venda(id),"+
+                        "FOREIGN KEY (produto_id) REFERENCES Produto(id)"+
+                ");";
+                stmt.executeUpdate(tabelaVendaProduto);
+                System.out.println("Tabela venda produto criado com sucesso!");
+
+                String tabelaPontos= "CREATE TABLE IF NOT EXISTS Troca_Pontos ("+
+                        "id SERIAL PRIMARY KEY,"+
+                        "cliente_id INT NOT NULL, "+
+                        "produto_id INT NOT NULL," +
+                        "quantidade INT NOT NULL,"+
+                        "pontos_utilizados INT NOT NULL,"+
+                        "data_troca TIMESTAMP DEFAULT CURRENT_TIMESTAMP,"+
+                        "FOREIGN KEY (cliente_id) REFERENCES Cliente(id),"+
+                        "FOREIGN KEY (produto_id) REFERENCES Produto(id)+"+");";
+
+                stmt.executeUpdate(tabelaPontos);
+                System.out.println("Tabela pontos criado com sucesso!");
+
+
             } catch (SQLException e) {
 
                 System.err.println("Erro SQL ao inicializar o banco de dados: " + e.getMessage());
